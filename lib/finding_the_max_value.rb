4df5c95ba8a84434b0bbee63bafcc5e73c08a711
David Molina <davidmolina2810@gmail.com>
2020-08-09 06:03:40 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
+#1. assign var max = nil
+#2. counter = 0
+#3. while counter < array.length
+  #4. if array[counter] > array[counter+1] and max < array[counter]
+    #5. max = array [counter]
+    #6. counter += 1
+  #7. else
+        #counter += 1
+  #8. return max
 def find_max_value(array)
   # Add your solution here
-end
